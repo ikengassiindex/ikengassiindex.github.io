5f5004dffe5d67c32a06ac52ecce0f602cda39ae
IkengaTest <c.berard@ikenga.eu>
2026-03-20 11:52:00 +0100

    Bump ssi-data v=44 + map.js v=56 (confidence_pct key names fixed: high/medium/low)

--- map.js
+++ map.js
@@ -1225,7 +1225,7 @@ if (!hasNested) {
     const basePath = options.basePath || '';
     Promise.all([
       fetch(basePath + 'grid-geo.json?v=16').then(r => r.json()),
-      fetch(basePath + 'ssi-data.json?v=43').then(r => r.json())
+      fetch(basePath + 'ssi-data.json?v=44').then(r => r.json())
     ]).then(([geo, ssi]) => {
       GEO = geo;
       SSI = ssi;
